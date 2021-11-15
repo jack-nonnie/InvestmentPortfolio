316976e3cd7b41b66e70830cde881f0f854d373d
Jack <60860781+jack-nonnie@users.noreply.github.com>
2021-11-15 16:00:25 -0500

    expanded on portfolio a lot

--- src/main/java/ee552/investment/portfolio/Portfolio.java
+++ src/main/java/ee552/investment/portfolio/Portfolio.java
@@ -3,9 +3,59 @@ package ee552.investment.portfolio;
 import java.util.ArrayList;
 
 public class Portfolio {
-    ArrayList<Stock> stocks = new ArrayList<Stock>();
-    ArrayList<Float> positions = new ArrayList<Float>();
-    ArrayList<Float> originalPrices = new ArrayList<Float>();
+    private ArrayList<Stock> stocks;
+    private ArrayList<Float> positions;
+    private ArrayList<Float> originalPrices;
+
+    public Portfolio() {
+        this.stocks = new ArrayList<Stock>();
+        this.positions = new ArrayList<Float>();
+        this.originalPrices = new ArrayList<Float>();
+    }
+
+    public Portfolio(ArrayList<Stock> stocks, ArrayList<Float> positions, ArrayList<Float> originalPrices) {
+        this.stocks = stocks;
+        this.positions = positions;
+        this.originalPrices = originalPrices;
+    }
+
+    public float getPercentChangeByIndex(int index) {
+        float currentValue = this.getCurrentValueOfAPositionByIndex(index);
+        float originalPrice = this.originalPrices.get(index);
+        return (currentValue - originalPrice) / originalPrice;
+    }
+
+    public int getSize() {
+        return this.stocks.size();
+    }
+
+    public float getCurrentValueOfAPositionByIndex(int index) {
+        if (index >= this.stocks.size() || index < 0) {
+            return -1;
+        }
+        return this.positions.get(index) * this.stocks.get(index).getCurrentPrice();
+    }
+
+    public float getOriginalPriceByIndex(int index) {
+        if (index >= this.originalPrices.size() || index < 0) {
+            return -1;
+        }
+        return this.originalPrices.get(index);
+    }
+
+    public float getPostionByIndex(int index) {
+        if (index >= this.positions.size() || index < 0) {
+            return -1;
+        }
+        return this.positions.get(index);
+    }
+
+    public Stock getStockByIndex(int index) {
+        if (index >= this.stocks.size() || index < 0) {
+            return null;
+        }
+        return this.stocks.get(index);
+    }
 
     public void updateStocks(ArrayList<Stock> stocks) {
         this.stocks = stocks;
--- src/main/java/ee552/investment/portfolio/Stock.java
+++ src/main/java/ee552/investment/portfolio/Stock.java
@@ -1,12 +1,12 @@
 package ee552.investment.portfolio;
 
 public class Stock {
-  public String ticker;
-  public String openPrice;
-  public String highPrice;
-  public String lowPrice;
-  public String currentPrice;
-  public String previousClosingPrice;
+  private String ticker;
+  private String openPrice;
+  private String highPrice;
+  private String lowPrice;
+  private String currentPrice;
+  private String previousClosingPrice;
 
   public Stock(String ticker, String open, String high, String low, String current, String prev) {
     this.ticker = ticker;
@@ -15,6 +15,58 @@ public class Stock {
     this.lowPrice = low;
     this.currentPrice = current;
     this.previousClosingPrice = prev;
+  }
+
+  public void setPreviousClosingPrice(String prev) {
+    this.previousClosingPrice = prev;
+  }
+
+  public void setCurrent(String current) {
+    this.currentPrice = current;
+  }
+
+  public void setLow(String low) {
+    this.lowPrice = low;
+  }
+
+  public void setHigh(String high) {
+    this.highPrice = high;
+  }
+
+  public void setOpen(String open) {
+    this.openPrice = open;
+  }
+
+  public void setPrices(String open, String high, String low, String current, String prev) {
+    this.openPrice = open;
+    this.highPrice = high;
+    this.lowPrice = low;
+    this.currentPrice = current;
+    this.previousClosingPrice = prev;
+  }
+
+  public String getTicker() {
+    return this.ticker;
+  }
+
+  public float getOpenPrice() {
+    return Float.parseFloat(this.openPrice);
+  }
+
+  public float getHighPrice() {
+    return Float.parseFloat(this.highPrice);
+  }
+
+  public float getLowPrice() {
+    return Float.parseFloat(this.lowPrice);
+  }
+
+  public float getCurrentPrice() {
+    return Float.parseFloat(this.currentPrice);
+  }
+
+  public float getPreviousClosingPrice() {
+    return Float.parseFloat(this.previousClosingPrice);
   }
 
   @Override
