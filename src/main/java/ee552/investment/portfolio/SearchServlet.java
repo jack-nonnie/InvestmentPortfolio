559415b528e18a8f0eb59a11679d01a29d779191
Jack <60860781+jack-nonnie@users.noreply.github.com>
2021-11-16 11:17:35 -0500

    First attempt at fin api call

--- src/main/java/ee552/investment/portfolio/SearchServlet.java
+++ src/main/java/ee552/investment/portfolio/SearchServlet.java
@@ -2,16 +2,27 @@ package ee552.investment.portfolio;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+
 @WebServlet("/search")
 public class SearchServlet extends HttpServlet {
+    @Autowired
+    private Environment env;
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println(env.getProperty("apiKey"));
         response.setContentType("text/html");
         PrintWriter writer = response.getWriter();
         writer.println("<html>");
@@ -38,6 +49,17 @@ public class SearchServlet extends HttpServlet {
         writer.println("</label>");
         writer.println("<input type='submit' value='Submit'/>");
         writer.println("<h2> " + ticker + "</h2>");
+        String url = "https://finnhub.io/api/v1/quote?symbol=" + ticker + "&token=" + env.getProperty("apiKey");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse.BodyHandler<String> handler = HttpResponse.BodyHandlers.ofString();
+        HttpRequest finnRequest = HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
+        try {
+            HttpResponse<String> finnResponse = client.send(finnRequest, handler);
+            System.out.println(finnResponse.body());
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         writer.println("</html>");
 
     }
