ed1315518ee7e0f687e311eab993933674f36c31
Jack <60860781+jack-nonnie@users.noreply.github.com>
2021-11-16 10:43:30 -0500

    Work on search

--- src/main/java/ee552/investment/portfolio/SearchServlet.java
+++ src/main/java/ee552/investment/portfolio/SearchServlet.java
@@ -12,14 +12,33 @@ import javax.servlet.http.HttpServletResponse;
 public class SearchServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
         PrintWriter writer = response.getWriter();
         writer.println("<html>");
         writer.println("<h1> Search </h1>");
+        writer.println("<form action= '/search' method='post'>");
+        writer.println("<label> Ticker");
+        writer.println("<input type='text' name='ticker'/>");
+        writer.println("</label>");
+        writer.println("<input type='submit' value='Submit'/>");
         writer.println("</html>");
     }
 
     @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String ticker = request.getParameter("ticker");
+        System.out.println(ticker);
+        response.setContentType("text/html");
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1> Search </h1>");
+        writer.println("<form action= '/search' method='post'>");
+        writer.println("<label> Ticker");
+        writer.println("<input type='text' name='ticker'/>");
+        writer.println("</label>");
+        writer.println("<input type='submit' value='Submit'/>");
+        writer.println("<h2> " + ticker + "</h2>");
+        writer.println("</html>");
 
     }
 
