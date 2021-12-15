3f47a78a3d80a3abad33705d73c5b9f77ee01d27
Tadao Shimura <tdshimura@gmail.com>
2021-12-15 02:27:00 -0700

    Autofill functionality + extras

--- src/main/java/com/app/main/controller/TradeController.java
+++ src/main/java/com/app/main/controller/TradeController.java
@@ -1,16 +1,19 @@
 package com.app.main.controller;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import java.util.Map;
 
 import com.app.main.model.Cash;
 import com.app.main.model.Trade;
+import com.app.main.model.Search;
 import com.app.main.service.CashService;
+import com.app.main.service.SearchService;
 import com.app.main.service.TradeService;
 
 @Controller
@@ -18,21 +21,28 @@ public class TradeController {
 
 	@Autowired
 	private TradeService tradeService;
+	@Autowired
+	private SearchService searchService;
 	@Autowired 
 	private CashService cashService;
 
-	@GetMapping("/trade")  // {id}")
-	public String trade(Model model) {// (@PathVariable ( value = "id") long id, Model model) {
+	@GetMapping("/trade/{id}")  // {id}")
+	public String trade(@PathVariable ( value = "id") String id, Model model) {
 		// create model attribute to bind form data
 		// Stock stock = stockService.getStockById(id);
-		Trade trade = new Trade();
+		Search search = new Search();
+		search.setStr(id);
 		// trade.setSymbol(stock.getSymbol());
+		Map<String, Object> stock = searchService.searchStock(search);
+		Trade trade = new Trade();
+		trade.setSymbol(id.toUpperCase());
+		trade.setPrice(stock.get("currentPrice").toString());
 		model.addAttribute("trade", trade);
 		return "trade";
 	}
 
 	@PostMapping("/enterTrade")
-	public String enterTrade(Model model, @ModelAttribute("trade") Trade trade ) {
+	public String enterTrade(Model model, @ModelAttribute("trade") Trade trade) {
 		// enter trade to database
 		trade.setSymbol(trade.getSymbol().toUpperCase());
 		double cashBalance = cashService.getBalance();
